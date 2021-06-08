f57f887bb641874c79d6b893e0e8a09abb6f5264
Adam Girard-McFadden <adam_m_20@hotmail.com>
2021-06-08 15:21:31 -0400

    usability and refactoring complete

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,18 @@
 "use strict";
-// Assignment code here
-// create all possible variables to be used as password
+/* -----------------
+Assignment code here
+------------------*/
+
+//function to iterate through CharacterCodes with "LOW" and "HIGH" values to generate arrays
 function arrayFromLowtoHigh(low, high) {
-  const array = [];
+  let array = [];
   for (let i = low; i <= high; i++) {
     array.push(i);
   }
   return array;
 }
 
+// create all possible variables to be used as password
 const lowercaseCharCodes = arrayFromLowtoHigh(97, 122);
 const uppercaseCharCodes = arrayFromLowtoHigh(65, 90);
 const numberCharCodes = arrayFromLowtoHigh(48, 57);
@@ -17,12 +21,11 @@ const specialCharCodes = arrayFromLowtoHigh(33, 47)
   .concat(arrayFromLowtoHigh(91, 96))
   .concat(arrayFromLowtoHigh(123, 126));
 
-let charCodes = numberCharCodes;
-let passLength2 = [];
-// Get references to the #generate element
-const generateBtn = document.querySelector("#generate");
+//create empty arrays to use in code
+let charCodes = [];
+let passwordLengthNumber = [];
 
-// create function to prompt and save user input for desired password length
+//function to prompt and save user input for desired password length
 const lengthChoice = function () {
   const passLength = window.prompt(
     "Choose the password's length requirement. It must be between 8 and 128 characters"
@@ -34,59 +37,79 @@ const lengthChoice = function () {
     lengthChoice();
   } else {
     let passwordLength = parseInt(passLength);
-    passLength2.push(passwordLength);
+    passwordLengthNumber.push(passwordLength);
+    alert("Your password will be " + passwordLength + " characters");
   }
 };
 
+//function to ask if user wants uppercase characters
 const upperCaseIncl = function () {
   let includeUppercase = window.confirm(
-    "Would you like to include uppercase characters?"
+    "Would you like to include 'UPPERCASE CHARACTERS'?"
   );
-  if (includeUppercase) charCodes = charCodes.concat(uppercaseCharCodes);
+
+  if (includeUppercase) {
+    charCodes = charCodes.concat(uppercaseCharCodes);
+    alert("You chose to include 'UPPERCASE CHARACTERS'");
+  } else {
+    alert("You have chosen to exclude 'UPPERCASE CHARACTERS'");
+  }
 };
 
+//function to ask if user wants lowercase characters
 const lowerCaseIncl = function () {
   let includeLowercase = window.confirm(
-    "Would you like to include lowercase characters?"
+    "Would you like to include 'LOWERCASE CHARACTERS'?"
   );
-  if (includeLowercase) charCodes = charCodes.concat(lowercaseCharCodes);
+  if (includeLowercase) {
+    charCodes = charCodes.concat(lowercaseCharCodes);
+    alert("You chose to include 'LOWERCASE CHARACTERS'");
+  } else {
+    alert("You chose to exclude 'LOWERCASE CHARACTERS'");
+  }
 };
 
+// function to ask if user wants numeric values
 const numericIncl = function () {
-  let includeNums = window.confirm("Would you like to include numbers?");
-  if (includeNums) charCodes = charCodes.concat(numberCharCodes);
+  let includeNums = window.confirm("Would you like to include NUMBERS?");
+  if (includeNums) {
+    charCodes = charCodes.concat(numberCharCodes);
+    alert("You chose to include 'NUMBERS'");
+  } else {
+    alert("You chose to exclude 'NUMBERS'");
+  }
 };
 
+// function to ask if user wants special characters
 const specialIncl = function () {
   let includeSpecial = window.confirm(
-    "Would you like to include special characters?"
+    "Would you like to include 'SPECIAL CHARACTERS' (#, %, & etc.) ?"
   );
-  if (includeSpecial) charCodes = charCodes.concat(specialCharCodes);
+  if (includeSpecial) {
+    charCodes = charCodes.concat(specialCharCodes);
+    alert("You chose to include 'SPECIAL CHARACTERS'");
+  } else {
+    alert("You chose to exclude 'SPECIAL CHARACTERS'");
+  }
 };
 
 //create function to pull answers from user and generate password
 const generatePassword = function () {
   const passwordCharacters = [];
-  for (let i = 0; i < passLength2; i++) {
+  for (let i = 0; i < passwordLengthNumber; i++) {
     const characterCode =
       charCodes[Math.floor(Math.random() * charCodes.length)];
     passwordCharacters.push(String.fromCharCode(characterCode));
   }
-
-  let newPasswordChar = passwordCharacters.join("");
-
-  let passwordText = document.querySelector("#password");
-  passwordText.value = newPasswordChar;
+  //join values into one continuous string
+  const newPasswordChar = passwordCharacters.join("");
+  //write generated password to text area box
+  const passwordText = document.querySelector("#password");
+  passwordText.textContent = newPasswordChar;
 };
 
-// Write password to the #password input
-var passwordText = document.querySelector("#password");
-// create randomizer that draws from passwordArray at chosen length and generate password to text area
-
-// Write password to the #password input
-
-// bring all small functions together to simplify event listener
-var clickPassword = function () {
+// bring all small functions together
+const clickPassword = function () {
   lengthChoice();
   upperCaseIncl();
   lowerCaseIncl();
@@ -95,23 +118,7 @@ var clickPassword = function () {
   generatePassword();
 };
 
+// Get references to the #generate element
+const generateBtn = document.querySelector("#generate");
 // Add event listener to generate button
 generateBtn.addEventListener("click", clickPassword);
-
-/*Set up object or Arrays for:
--Password length
--letters
--numbers
--special characters 
-
--Once requirement selected, generate button will RANDOMLY draw from each and display to user in random order (maybe).
-
--need event listener for button
--once pressed, will generate prompts for password criteria
--then will promt for password length between 8 and 128
--then prompted for character types:
-lowercase, uppercase, numberic, and or special caracters
--input should be validated and at least one caracter type should be selected
--once all is answered, password is generated that matches the user selection
--password is either generated to an alert window or written on the page
-*/
